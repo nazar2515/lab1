01258993f44a653a9eb064e10c0f8676f1c25841
nazar2515 <nazar.temchenko05@gmail.com>
2024-11-19 22:08:26 +0200

    Add files via upload

---
+++ untitled7/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BreakThread breakThread = new BreakThread();
+
+        new MainThread(0, breakThread).start();
+        new MainThread(3, breakThread).start();
+        new MainThread(5, breakThread).start();
+
+        new MainThread(2, breakThread).start();
+        new MainThread(4, breakThread).start();
+        new MainThread(6, breakThread).start();
+
+        new MainThread(2, breakThread).start();
+        new MainThread(3, breakThread).start();
+        new MainThread(4, breakThread).start();
+
+        new Thread(breakThread).start();
+    }
+}
